f50414634dd72ec3077788288a39ef1b4d5613f0
Adi Oporanu <aoporanu@gmail.com>
2020-10-06 23:42:44 +0300

    InvoiceAPIController store and InvoiceStoreRequest

--- app/Http/Controllers/API/InvoiceAPIController.php
+++ app/Http/Controllers/API/InvoiceAPIController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\API;
 
 use App\Invoice;
+use App\Order;
+use App\OrderItem;
+use App\Http\Requests\InvoiceStoreRequest;
+use App\Http\Requests\InvoiceUpdateRequest;
 use App\Http\Resources\InvoiceCollection;
 use App\Http\Resources\InvoiceResource;
 use Illuminate\Http\Request;
@@ -21,10 +25,23 @@ class InvoiceAPIController extends Controller
 
     public function store(InvoiceStoreRequest $request)
     {
+        // mefinks this should go inside the InvoiceStoreRequest
+        $order = Order::where('id', $request->order_id);
+        switch($order->status) {
+            case 'processed':
+                return response()->json(['success' => false, 'message' => 'The order has already been processed']);
+            break;
+            case 'rejected':
+                return response()->json(['success' => false, 'message' => 'The order has been rejected by someone at the office']);
+            break;
+            case 'cancelled':
+                return response()->json(['success' => false, 'message' => 'The order has been cancelled by the client']);
+            break;
+        }
         return new InvoiceResource(Invoice::create($request->all()));
     }
 
-    public function update(InvoiceUpdateRequest $request)
+    public function update(InvoiceUpdateRequest $request, Invoice $invoice)
     {
         $invoice->update($request->all());
 
