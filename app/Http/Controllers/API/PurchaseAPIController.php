9b82a3add140e526e2495f32e4fd1831254f7f17
Adi Oporanu <aoporanu@gmail.com>
2020-10-28 01:04:01 +0200

    Purchases screwed, need to think more on the batches thing

--- app/Http/Controllers/API/PurchaseAPIController.php
+++ app/Http/Controllers/API/PurchaseAPIController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\PurchaseCollection;
 use App\Http\Resources\PurchaseResource;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\PurchasedItems;
 use Illuminate\Support\Facades\DB;
 
 class PurchaseAPIController extends Controller
@@ -24,14 +25,32 @@ class PurchaseAPIController extends Controller
 
     public function store(PurchaseStoreRequest $request)
     {
-        DB::beginTransaction();
-        Purchase::create($request->all());
-        if (!DB::commit()) {
-            DB::rollBack();
-            return new PurchaseResource(Purchase::create($request->all()));
+        // DB::beginTransaction();
+        $purchase = Purchase::create($request->all());
+        if ($purchase) {
+            $total = 0;
+            foreach ($request->get('purchase_items') as $item) {
+                if ($purchase->discount != 0) {
+                    $item['selling_cost'] = $item['selling_cost'] * ((100 - $purchase->discount) / 100);
+                    $item['purchase_cost'] = $item['purchase_cost'] * ((100 - $purchase->discount) / 100);
+                    
+                }
+                $total += (double)$item['selling_cost'] * $item['qty'];
+                $purchasedItems[] = $item;
+            }
+            $purchase->total = $total;
+            $purchase->save();
+            $purchase->purchasedItems()->create($purchasedItems);
+        } else {
+            return response()->json(['success' => false, 'message' => 'Could not commit the purchase']);
         }
-
-        return response()->json(['success' => false, 'message' => 'Could not commit the purchase']);
+        return response()->json([
+            'success' => true, 
+            'message' => 'Purchase successfully stored', 
+            'purchase' => [
+                 'object' => new PurchaseResource($purchase)
+            ]
+        ]);
     }
 
     public function update(Request $request, Purchase $purchase)
