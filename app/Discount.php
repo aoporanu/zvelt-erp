434176256a5c60bd4148a6157c33e80f3d732612
Adi Oporanu <aoporanu@gmail.com>
2020-10-31 19:23:21 +0200

    Fix #10

--- app/Discount.php
+++ app/Discount.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Discount extends Model
 {
-    //
+    public function applicableTo(Item $item)
+    {
+        $brand = $item->brand;
+        // check if there is a discount for the given brand on the current shop
+    }
 }
