d5c2e9e83e9bd038ac10eb83db00c729a74d7e62
aoporanu <aoporanu@gmail.com>
2021-01-10 13:57:41 +0200

    New item layout

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -23,6 +23,15 @@
                     </select>
                 </div>
                 <div class="form-group">
+                    <label for="brand_id" class="control-label">Brand</label>
+                    <select name="brand_id" id="brand_id" class="form-control">
+                        <option value="">Select brand</option>
+                        @foreach($brands as $brand)
+                        <option value="{{ $brand->id }}">{{ $brand->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
                     <label for="vat" class="control-label">VAT</label>
                     <input type="text" class="form-control" id="vat" value="{{ old('vat') }}" />
                 </div>
@@ -47,7 +56,25 @@
                             @endforeach
                     </select>
                 </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-success" value="Save item" />
+                </div>
             </form>
         </div>
     </div>
+
+@endsection
+@push('scripts')
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#packaging').select2();
+    $('#category_id').select2();
+    $('#brand_id').select2();
+});
+</script>
+@endpush
+@section('styles')
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
 @endsection
