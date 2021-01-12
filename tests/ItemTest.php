405c94c3e48c052a08808217eb18eee0e60dcaf8
aoporanu <aoporanu@gmail.com>
2021-01-13 01:06:38 +0200

    Tests

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -3,13 +3,15 @@
 namespace Tests;
 
 use App\Item;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\TestCase;
 
 class ItemTest extends TestCase
 {
+    use RefreshDatabase;
 
-    public function testCategory()
-    {
-
-    }
+//    public function testCategory()
+//    {
+//        $this->assertTrue(true);
+//    }
 }
