c9f2b37d51a327142995dafac364e140155ffc6a
aoporanu <aoporanu@gmail.com>
2021-01-12 11:52:31 +0200

    Updated controllers, App\{Model} was no longer a valid namespace for v8

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserRegisterRequest;
-use App\User;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Passport\Client;
