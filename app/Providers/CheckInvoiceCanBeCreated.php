a7f1a011069fc43adf7829a4bf7e6dd016399014
aoporanu <aoporanu@gmail.com>
2021-01-19 00:06:30 +0200

    Observers and providers

---
+++ app/Providers/CheckInvoiceCanBeCreated.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use App\Providers\InvoiceCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class CheckInvoiceCanBeCreated
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  InvoiceCreated  $event
+     * @return void
+     */
+    public function handle(InvoiceCreated $event)
+    {
+        //
+    }
+}
