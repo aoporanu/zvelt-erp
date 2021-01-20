783ec725c79dd970c39af9f5dc4a0dc17f97fa9d
aoporanu <aoporanu@gmail.com>
2021-01-21 00:09:26 +0200

    added the transfer request validation

---
+++ app/Http/Requests/TransferPurchaseRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TransferPurchaseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "item_name"     => 'required',
+            "from_warehose"  => 'required|exists:warehouses,id',
+            "from_location"   => 'required|exists:locations,id',
+            "to_warehouse"  => 'required|exists:warehouses,id',
+            "to_location"   => 'required|exists:locations,id'
+        ];
+    }
+}
