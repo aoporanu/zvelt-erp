b8fb263cb6c0b060370e7a32491b3d8ea43acacc
Adi Oporanu <aoporanu@gmail.com>
2022-02-26 02:28:18 +0200

    Translations in part

---
+++ resources/lang/en/warehouses.php
@@ -0,0 +1,10 @@
+<?php
+  return [
+    'create' => [
+      'label' => 'Warehouse name'
+    ],
+    'store' => [
+      'success' => 'The warehouse has been created',
+      'error' => 'Could not create the warehouse'
+    ]
+  ];
