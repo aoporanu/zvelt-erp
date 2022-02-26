ba287106afd48e247d1a9824c5afd492b038756f
Adi Oporanu <aoporanu@gmail.com>
2022-02-26 10:30:29 +0200

    Display warehouse name in datatable

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -2,49 +2,48 @@
 
 @section('content')
 <div class="card full-height">
-<div class="card-body">
-  <table class="table table-bordered data-table">
-<thead>
-<tr>
-  <th>ID</th>
-  <th>Name</th>
-  <th>Type</th>
-  <th>On warehouse</th>
-<th>actions</th>
-</tr>
-</thead>
-</table>
+  <div class="card-body">
+    <table class="table table-bordered data-table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Type</th>
+          <th>On warehouse</th>
+          <th>actions</th>
+        </tr>
+      </thead>
+    </table>
   </div>
 </div>
 <script type="text/javascript">
-$(document).ready(function () {
-  $('.data-table').DataTable({
-  processing: true,
-serverSide: true,
-ajax: "{{ route('locations.index') }}",
-columns: [
-  {
-  data: 'id',
-name: 'id',
-searchable: false
-},
-{
-  data: 'name',
-  name: 'name'
-},
-{
-  data: 'type',
-  name: 'type'
-},
-{
-  data: 'warehouse_id',
-  name: 'warehouse_id'
-}
-],
-select: true,
-responsive: true
-})
-})
+  $(document).ready(function() {
+    $('.data-table').DataTable({
+      processing: true,
+      serverSide: true,
+      ajax: "{{ route('locations.index') }}",
+      columns: [{
+          data: 'id',
+          name: 'id',
+          searchable: false
+        },
+        {
+          data: 'name',
+          name: 'name'
+        },
+        {
+          data: 'type',
+          name: 'type'
+        },
+        {
+          data: 'warehouse.name',
+          name: 'warehouse'
+        }
+      ],
+      select: true,
+      responsive: true
+    })
+  })
 </script>
 @endsection
 
