b2ef34e6a5c6329313d92f59664eae49ccf7c92a
Adi Oporanu <aoporanu@gmail.com>
2022-02-26 02:27:48 +0200

    Namespace fixes

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,20 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PurchasesController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\WarehouseController;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\PackagingController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\InvoicesController;
+use App\Http\Controllers\FinancialsController;
 
 Route::resource('items', ItemController::class);
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 Route::get(
     'purchase/stocks', 
     [PurchasesController::class, 'stocks']
-)
-    ->name('purchase.stocks');
+)->name('purchase.stocks');
 Route::get(
     'purchase/transfer',
     [
