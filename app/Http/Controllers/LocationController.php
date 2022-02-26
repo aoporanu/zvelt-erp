a53ca550d36b1fa27b17038ffdf8e94a632bad26
Adi Oporanu <aoporanu@gmail.com>
2022-02-26 10:27:02 +0200

    Edit and update crud methods for the locations. Also get warehouse name not just id

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -9,19 +9,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use App\Http\Requests\LocationStoreRequest;
 use App\Models\Location;
 use Yajra\Datatables\Datatables;
+use App\Http\Requests\LocationUpdateRequest;
 use \DB;
+
 class LocationController extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+  use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
   public function index()
   {
     $pageTitle = 'Locations';
     if (request()->ajax()) {
-      $model = Location::select(['id', 'name', 'type', 'warehouse_id']);
+      $model = Location::with('warehouse')->select(['id', 'name', 'type', 'warehouse_id']);
       return Datatables::of($model)
         ->addIndexColumn()
         ->addColumn('action', function ($row) {
           $btn = '<a href="javascript:void(0)" class="view btn btn-primary btn-sm">View</a>';
+          $btn .= '<a href="' . route('locations.edit', $row->id) . '" class="edit btn btn-sm btn-warning">Edit</a>';
+          $btn .= '<form method="post" action="' . route('locations.destroy', $row->id) . '">' . method_field('delete') . '<button class="btn btn-sm btn-danger">Delete</button></form>';
           return $btn;
         })
         ->rawColumns(['action'])->make(true);
@@ -42,5 +46,16 @@ class LocationController extends BaseController
     $location = $locationStoreRequest->validated();
 
     Location::create($location);
+  }
+
+  public function edit(Location $location)
+  {
+    $pageTitle = __('locations.update.title');
+    return view('locations.create', compact('location', 'pageTitle'));
+  }
+
+  public function update(Location $location, LocationUpdateRequest $locationUpdateRequest)
+  {
+    $location->update($locationUpdateRequest->validated());
   }
 }//end class
