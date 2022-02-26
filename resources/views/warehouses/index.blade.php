388877e6552685d8202bc884caf9415b822f9090
Adi Oporanu <aoporanu@gmail.com>
2022-02-26 02:22:39 +0200

    Warehouse index with working datatable

--- resources/views/warehouses/index.blade.php
+++ resources/views/warehouses/index.blade.php
@@ -1,7 +1,27 @@
 @extends('layouts.admin')
 
 @section('content')
-
+    <div class="card full-height">
+        <div class="card-body">
+            <a href="{{ route('purchase.create') }}" role="button" class="btn btn-primary">
+                <i class="fa fa-plus-circle"></i>
+            </a>
+            <table class="table table-bordered data-table">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>uniqid</th>
+                        <th>value</th>
+                        <th>total</th>
+                        <th>discount</th>
+                        <th>supplier</th>
+                        <th>purchased on</th>
+                        <th>actions</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
 @endsection
 
 @section('sidebar')
