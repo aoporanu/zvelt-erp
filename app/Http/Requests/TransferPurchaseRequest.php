427c4f8341165f798c230a58f22bbf9de0e50fc6
Adi Oporanu <aoporanu@gmail.com>
2022-02-27 20:45:11 +0200

    TransferPurchaseRequest request

--- app/Http/Requests/TransferPurchaseRequest.php
+++ app/Http/Requests/TransferPurchaseRequest.php
@@ -10,9 +10,11 @@
  * @license  http://opensource.org/licenses/gpl-license.php  GNU Public License
  * @link     http://zvelt-erp.com
  */
+
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+
 /**
  * The transfer request class
  * php version ^8.0
@@ -25,36 +27,31 @@ use Illuminate\Foundation\Http\FormRequest;
  */
 class TransferPurchaseRequest extends FormRequest
 {
+  protected $errorBag = 'transferBag';
 
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return boolean
+   */
+  public function authorize(): bool
+  {
+    return true;
+  } //end authorize()
 
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return boolean
-     */
-    public function authorize(): bool
-    {
-        return true;
-
-    }//end authorize()
-
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            'item_id'       => 'required|exists:items,id',
-            'from_warehose' => 'required|exists:warehouses,id',
-            'from_location' => 'required|exists:locations,id',
-            'to_warehouse'  => 'required|exists:warehouses,id',
-            'to_location'   => 'required|exists:locations,id',
-        ];
-
-    }//end rules()
-
-
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'item_id'       => 'required|exists:items,id',
+      'from_warehose' => 'required|exists:warehouses,id',
+      'from_location' => 'required|exists:locations,id',
+      'to_warehouse'  => 'required|exists:warehouses,id',
+      'to_location'   => 'required|exists:locations,id',
+    ];
+  } //end rules()
 }//end class
