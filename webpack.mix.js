c2ce62d53f7d06081f1d4c05abc287e61c1db429
Adi Oporanu <aoporanu@gmail.com>
2022-02-27 20:34:35 +0200

    new webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,37 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+  .combine([
+    'resources/css/bootstrap.min.css', 
+    'resources/css/atlantis.min.css', 
+    'resources/css/fonts.min.css', 
+    'resources/css/info-box.css',
+    'https://cdn.datatables.net/autofill/2.3.5/css/autoFill.dataTables.min.css',
+    'https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css'
+  ], 'resources/css/app.css')
+  .scripts([
+  'resources/js/core/bootstrap.min.js',
+  'https://code.jquery.com/ui/1.12.1/jquery-ui.js',
+  'resources/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js',
+  'resources/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js',
+  'resources/js/plugin/chart.js/chart.min.js',
+  'resources/js/plugin/jquery.sparkline/jquery.sparkline.min.js',
+  'resources/js/plugin/chart-circle/circles.min.js',
+  'https://editor.datatables.net/extensions/Editor/js/dataTables.editor.min.js',
+  'https://cdn.datatables.net/autofill/2.3.5/js/dataTables.autoFill.min.js',
+  'https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js',
+  'https://cdn.datatables.net/buttons/1.6.5/js/buttons.colVis.min.js',
+  'https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js',
+  'https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js',
+  'resources/js/plugin/bootstrap-notify/bootstrap-notify.min.js',
+  'resources/js/atlantis.min.js',
+  'resources/js/setting-demo.js'
+  ], 'resources/js/app.js')
+  .postCss('resources/css/app.css', 'public/css', [
+    //
+  ]).options({
+    processCssUrls: false
+  })
