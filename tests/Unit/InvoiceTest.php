7d45cf81ca43f99e4729e4b85fb96588ee13198c
Adi Oporanu <aoporanu@gmail.com>
2022-02-28 22:39:33 +0200

    InvoiceTest update

--- tests/Unit/InvoiceTest.php
+++ tests/Unit/InvoiceTest.php
@@ -15,127 +15,128 @@ use Tests\TestCase;
 
 class InvoiceTest extends TestCase
 {
-    use RefreshDatabase;
+  use RefreshDatabase;
 
 
-    /**
-     * A basic unit test example.
-     *
-     * @test
-     * @return void
-     */
-    public function an_agent_can_cash_his_own_invoice()
-    {
-        $this->withoutExceptionHandling();
-        User::factory()->create();
-        Client::factory()->create();
-        Shop::factory()->create();
-        Warehouse::factory()->create();
-        Order::factory()->create();
-        $invoice = Invoice::factory()->create();
-        Ledger::factory()->create();
-        $user     = User::first();
-        $receipts = Receipt::factory()->times(50)->create();
+  /**
+   * A basic unit test example.
+   *
+   * @test
+   * @return void
+   */
+  public function an_agent_can_cash_his_own_invoice()
+  {
+    $this->withoutExceptionHandling();
+    User::factory()->create();
+    Client::factory()->create();
+    Shop::factory()->create();
+    Warehouse::factory()->create();
+    Order::factory()->create();
+    $invoice = Invoice::factory()->create();
+    Ledger::factory()->create();
+    $user     = User::first();
+    $receipts = Receipt::factory()->times(50)->create();
 
-        $this->assertTrue($invoice->cash($user, $invoice->total));
-        $receipt = Receipt::where('invoice_id', $invoice->id)->first();
-    }//end an_agent_can_cash_his_own_invoice()
-
-
-    /**
-     * @test
-     */
-    public function an_agent_can_cash_partially_an_invoice()
-    {
-        $this->withoutExceptionHandling();
-        User::factory()->create();
-        Client::factory()->create();
-        Shop::factory()->create();
-        Warehouse::factory()->create();
-        Order::factory()->create();
-        $invoice = Invoice::factory()->create();
-        Ledger::factory()->create();
-        $user     = User::first();
-        $receipts = Receipt::factory()->times(50)->create();
-
-        $this->assertTrue($invoice->cash($user, ($invoice->total / 2)));
-        $receipt = Receipt::where('invoice_id', $invoice->id)->first();
-        $this->assertNotNull($invoice->amount_left);
-    }//end an_agent_can_cash_partially_an_invoice()
+    $this->assertTrue($invoice->cash($user, $invoice->total));
+    $receipt = Receipt::where('invoice_id', $invoice->id)->first();
+    $this->assertNotNull($receipt);
+  } //end an_agent_can_cash_his_own_invoice()
 
 
-    public function test_user_can_cash_invoice()
-    {
-        $this->withoutExceptionHandling();
-        $this->create_models();
-        $user = User::factory()->create();
+  /**
+   * @test
+   */
+  public function an_agent_can_cash_partially_an_invoice()
+  {
+    $this->withoutExceptionHandling();
+    User::factory()->create();
+    Client::factory()->create();
+    Shop::factory()->create();
+    Warehouse::factory()->create();
+    Order::factory()->create();
+    $invoice = Invoice::factory()->create();
+    Ledger::factory()->create();
+    $user     = User::first();
+    $receipts = Receipt::factory()->times(50)->create();
 
-        $invoice = Invoice::factory()->create();
-        $invoice->cash($user);
-        $this->assertNotEquals($invoice->total, $invoice->amount_left);
-    }//end test_user_can_cash_invoice()
-
-
-    public function test_user_can_partially_cash_invoice()
-    {
-        $this->withoutExceptionHandling();
-        $this->create_models();
-        $user = User::factory()->create();
-
-        $invoice = Invoice::factory()->create();
-        $invoice->cash($user, ($invoice->total / 2));
-        $this->assertNotEquals($invoice->total, $invoice->amount_left);
-    }//end test_user_can_partially_cash_invoice()
+    $this->assertTrue($invoice->cash($user, ($invoice->total / 2)));
+    $receipt = Receipt::where('invoice_id', $invoice->id)->first();
+    $this->assertNotNull($invoice->amount_left);
+  } //end an_agent_can_cash_partially_an_invoice()
 
 
-    public function test_another_user_can_cash_invoice()
-    {
-        $this->withoutExceptionHandling();
-        $this->create_models();
-        $user = User::factory()->create();
+  public function test_user_can_cash_invoice()
+  {
+    $this->withoutExceptionHandling();
+    $this->create_models();
+    $user = User::factory()->create();
 
-        $invoice = Invoice::factory()->create();
-        $user1   = User::factory()->create();
-        $invoice->cash($user1);
-        $ledger   = Ledger::where('user_id', $user1->id)->first();
-        $receipts = Receipt::where('ledger_id', $ledger->id)->first();
-        $this->assertNotEquals($invoice->total, $invoice->amount_left);
-    }//end test_another_user_can_cash_invoice()
+    $invoice = Invoice::factory()->create();
+    $invoice->cash($user);
+    $this->assertNotEquals($invoice->total, $invoice->amount_left);
+  } //end test_user_can_cash_invoice()
 
 
-    public function test_another_user_can_partially_cash_another_invoice()
-    {
-        $this->withoutExceptionHandling();
-        $this->create_models();
-        $user = User::factory()->create();
+  public function test_user_can_partially_cash_invoice()
+  {
+    $this->withoutExceptionHandling();
+    $this->create_models();
+    $user = User::factory()->create();
 
-        $invoice = Invoice::factory()->create();
-        $user1   = User::factory()->create();
-        $invoice->cash($user1, ($invoice->total / 2));
-        $ledger   = Ledger::where('user_id', $user1->id)->first();
-        $receipts = Receipt::where('ledger_id', $ledger->id)->first();
-        $this->assertNotEquals($invoice->total, $invoice->amount_left);
-    }//end test_another_user_can_partially_cash_another_invoice()
+    $invoice = Invoice::factory()->create();
+    $invoice->cash($user, ($invoice->total / 2));
+    $this->assertNotEquals($invoice->total, $invoice->amount_left);
+  } //end test_user_can_partially_cash_invoice()
 
 
-    /**
-     * @test
-     */
-    public function it_tests_if_on_creating_an_invoice_the_customer_ceil_is_reduced()
-    {
-        $this->create_models();
-        $invoice = Invoice::factory()->create();
-        $this->assertNotEquals($invoice->client->ceil, $invoice->total);
-    }//end it_tests_if_on_creating_an_invoice_the_customer_ceil_is_reduced()
+  public function test_another_user_can_cash_invoice()
+  {
+    $this->withoutExceptionHandling();
+    $this->create_models();
+    $user = User::factory()->create();
+
+    $invoice = Invoice::factory()->create();
+    $user1   = User::factory()->create();
+    $invoice->cash($user1);
+    $ledger   = Ledger::where('user_id', $user1->id)->first();
+    $receipts = Receipt::where('ledger_id', $ledger->id)->first();
+    $this->assertNotEquals($invoice->total, $invoice->amount_left);
+  } //end test_another_user_can_cash_invoice()
 
 
-    private function create_models(): void
-    {
-        User::factory()->create();
-        Client::factory()->create();
-        Shop::factory()->create();
-        Warehouse::factory()->create();
-        Order::factory()->create();
-        Ledger::factory()->create();
-    }//end create_models()
+  public function test_another_user_can_partially_cash_another_invoice()
+  {
+    $this->withoutExceptionHandling();
+    $this->create_models();
+    $user = User::factory()->create();
+
+    $invoice = Invoice::factory()->create();
+    $user1   = User::factory()->create();
+    $invoice->cash($user1, ($invoice->total / 2));
+    $ledger   = Ledger::where('user_id', $user1->id)->first();
+    $receipts = Receipt::where('ledger_id', $ledger->id)->first();
+    $this->assertNotEquals($invoice->total, $invoice->amount_left);
+  } //end test_another_user_can_partially_cash_another_invoice()
+
+
+  /**
+   * @test
+   */
+  public function it_tests_if_on_creating_an_invoice_the_customer_ceil_is_reduced()
+  {
+    $this->create_models();
+    $invoice = Invoice::factory()->create();
+    $this->assertNotEquals($invoice->client->ceil, $invoice->total);
+  } //end it_tests_if_on_creating_an_invoice_the_customer_ceil_is_reduced()
+
+
+  private function create_models(): void
+  {
+    User::factory()->create();
+    Client::factory()->create();
+    Shop::factory()->create();
+    Warehouse::factory()->create();
+    Order::factory()->create();
+    Ledger::factory()->create();
+  } //end create_models()
 }//end class
