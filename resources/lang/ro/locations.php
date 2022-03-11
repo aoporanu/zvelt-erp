988c860287b3884fc127f1cd3435563e856e3590
Adi Oporanu <aoporanu@gmail.com>
2022-03-11 22:54:01 +0200

    i18n

---
+++ resources/lang/ro/locations.php
@@ -0,0 +1,9 @@
+<?php
+  return [
+  'create' => [
+    'label' => 'Location name',
+    'type' => 'Location type',
+    'warehouse' => 'Location\'s warehouse',
+    'select-warehouse' => 'Please select warehouse'
+  ],
+];
