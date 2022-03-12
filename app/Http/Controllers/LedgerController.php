cdb7d77f4f93b66d58837eb7a7cf1571f6c28f2b
Adi Oporanu <aoporanu@gmail.com>
2022-03-12 23:11:35 +0200

    Added LedgerService to LedgerController

--- app/Http/Controllers/LedgerController.php
+++ app/Http/Controllers/LedgerController.php
@@ -6,6 +6,7 @@ use App\Models\Ledger;
 use App\Http\Requests\LedgerStoreRequest;
 use App\Http\Requests\LedgerDeleteRequest;
 use App\Http\Requests\LedgerUpdateRequest;
+use App\Services\LedgerService;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -13,66 +14,66 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class LedgerController extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+  use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+  private LedgerService $ledgerService;
 
 
-    public function __construct()
-    {
-        parent::__construct();
-
-    }//end __construct()
-
-
-    public function index()
-    {
-        return view('ledgers.index');
-
-    }//end index()
+  public function __construct(LedgerService $ledgerService)
+  {
+    parent::__construct();
+    $this->ledgerService = $ledgerService;
+  } //end __construct()
 
 
-    public function show(Ledger $ledger)
-    {
-        return view('ledgers.show', compact('ledger'));
-
-    }//end show()
-
-
-    public function create()
-    {
-        return view('ledgers.create');
-
-    }//end create()
+  public function index()
+  {
+    $pageTitle = __('ledgers.pages.index.title');
+    return view('ledgers.index', compact('pageTitle'));
+  } //end index()
 
 
-    public function store(LedgerStoreRequest $request)
-    {
-        // code...
-
-    }//end store()
-
-
-    public function edit(Ledger $ledger)
-    {
-        return view('ledgers.edit', compact('ledger'));
-
-    }//end edit()
+  public function show(Ledger $ledger)
+  {
+    $pageTitle = __('ledgers.pages.show.title');
+    return view('ledgers.show', compact('ledger', 'pageTitle'));
+  } //end show()
 
 
-    public function update(Ledger $ledger, LedgerUpdateRequest $request)
-    {
+  public function create()
+  {
+    $pageTitle = __('ledgers.pages.create.title');
+    return view('ledgers.create', compact('pageTitle'));
+  } //end create()
 
-    }//end update()
+
+  public function store(LedgerStoreRequest $request)
+  {
+    (new Ledger)->create($request->validated());
+  } //end store()
 
 
-    public function destroy(Ledger $ledger, LedgerDeleteRequest $request)
-    {
-        if ($ledger->user->role != 'admin' || $ledger->user->role != 'operator') {
-            return false;
-        }
+  public function edit(Ledger $ledger)
+  {
+    $pageTitle = __('ledgers.pages.edit.title');
+    return view('ledgers.edit', compact('ledger', 'pageTitle'));
+  } //end edit()
 
-        $ledger->delete();
 
-    }//end destroy()
+  public function update(Ledger $ledger, LedgerUpdateRequest $request)
+  {
+    $ledger->update($request->validated());
+  } //end update()
+
+
+  public function destroy(Ledger $ledger, LedgerDeleteRequest $request)
+  {
+    if ($ledger->user->role != 'admin' || $ledger->user->role != 'operator') {
+      return false;
+    }
+
+    $ledger->delete();
+  } //end destroy()
 
 
 }//end class
