a1001a23d706b0972cfdd19b784842f612f27da6
Adi Oporanu <aoporanu@gmail.com>
2022-03-12 23:12:50 +0200

    Ledger pages translations

---
+++ resources/lang/en/ledgers.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+  'pages' => [
+    'index' => [
+      'title' => 'Ledgers',
+    ],
+    'show' => [
+      'title' => 'Ledger view',
+    ],
+    'create' => [
+      'title' => 'Create ledger',
+    ],
+    'edit' => [
+      'title' => 'Edit ledger'
+    ],
+  ], 
+];
